1fb6fb505d62c2affc705a3246edbbbd20ae7c9d
3s777 <3s777@rambler.ru>
2023-03-25 16:59:57 +0300

    DS init

---
+++ app/Console/Commands/RefreshlCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class RefreshlCommand extends Command
+{
+    protected $signature = 'shop:refresh';
+
+    protected $description = 'Refresh';
+
+    public function handle(): int
+    {
+        if(app()->isProduction()) {
+            return self::FAILURE;
+        }
+
+        $this->call('cache:clear');
+
+        Storage::deleteDirectory('images/products');
+        Storage::deleteDirectory('images/brands');
+
+        $this->call('migrate:fresh', [
+            '--seed' => true
+        ]);
+
+        return self::SUCCESS;
+    }
+}
