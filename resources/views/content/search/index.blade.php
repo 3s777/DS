40e5c080303cf08a0792cae6fe64bc205f5ded74
3s777 <3s777@rambler.ru>
2023-10-27 20:36:05 +0300

    add grid to search

--- resources/views/content/search/index.blade.php
+++ resources/views/content/search/index.blade.php
@@ -5,9 +5,11 @@
 @section('content')
     <div class="container">
         <div class="content carrier">
-
+            <x-grid type="container">
+                <x-grid.col lg="6" xl="6" md="6" sm="12">
             Search
-
+                </x-grid.col>
+            </x-grid>
         </div>
     </div>
 
