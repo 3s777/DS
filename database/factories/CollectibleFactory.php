4907c7368eb1389862e2a8f917eccdc7dddae85d
3s777 <3s777@rambler.ru>
2024-08-10 16:51:00 +0300

    add Collectible and GameMedia

--- database/factories/CollectibleFactory.php
+++ database/factories/CollectibleFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CollectableItem>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Collectible>
  */
-class CollectableItemFactory extends Factory
+class CollectibleFactory extends Factory
 {
     /**
      * Define the model's default state.
--- database/migrations/2024_08_10_134842_create_game_media_table.php
+++ database/migrations/2024_08_10_134842_create_game_media_table.php
@@ -4,16 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('collectable_items', function (Blueprint $table) {
+        Schema::create('game_media', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->jsonb('properties');
             $table->timestamps();
         });
     }
@@ -23,8 +22,8 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-        if (! app()->isProduction()) {
-            Schema::dropIfExists('collectable_items');
+        if(!app()->isProduction()) {
+                Schema::dropIfExists('game_media');
         }
     }
 };
