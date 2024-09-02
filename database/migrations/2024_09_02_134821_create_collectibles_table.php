4d5d170d19849b402070cdf111165f0e81d6355d
3s777 <3s777@rambler.ru>
2024-09-02 18:44:51 +0300

    add Shelf

--- database/migrations/2024_09_02_134821_create_collectibles_table.php
+++ database/migrations/2024_09_02_134821_create_collectibles_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Shelf;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,17 @@ return new class extends Migration
     {
         Schema::create('collectibles', function (Blueprint $table) {
             $table->id();
+            $table->ulid('ulid');
+            $table->string('name');
+            $table->string('article_number')->nullable();
+            $table->string('condition');
+            $table->foreignIdFor(Shelf::class)->constrained();
+            $table->jsonb('kit')->nullable();
+            $table->morphs('collectable');
+            $table->string('thumbnail')->nullable();
+            $table->jsonb('description')->nullable();
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -23,7 +35,7 @@ return new class extends Migration
     public function down(): void
     {
         if(!app()->isProduction()) {
-                Schema::dropIfExists('collectibles');
+            Schema::dropIfExists('collectibles');
         }
     }
 };
