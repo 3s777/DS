74cb23c7330d85fe4911ad4765d890b83e90cebf
3s777 <3s777@rambler.ru>
2024-09-09 09:02:19 +0300

    update vite combine media query rules

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-// import viteJoinMediaQueries from 'vite-join-media-queries';
 import postcssCombineMediaQuery from 'postcss-combine-media-query'
 import cssmqpacker from 'css-mqpacker';
 import sortMediaQueries from 'postcss-sort-media-queries';
@@ -24,8 +23,6 @@ export default defineConfig({
         postcss: {
             plugins: [
                 sortMediaQueries({sort: 'desktop-first'}),
-                // cssmqpacker({ sort: true }),
-                // postcssCombineMediaQuery
             ],
         },
         // preprocessorOptions: {
@@ -39,9 +36,5 @@ export default defineConfig({
             input: ['resources/scss/app.scss', 'resources/scss/admin.scss', 'resources/js/app.js', 'resources/js/scripts.js'],
             refresh: true,
         }),
-        // viteJoinMediaQueries({
-        //     paths2css: ['./public/build/assets'],
-        //     cssnanoConfig: { preset: 'default' },
-        // }),
     ],
 });
