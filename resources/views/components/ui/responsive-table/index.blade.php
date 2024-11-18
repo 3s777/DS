fb85219dbb382caa074900b135b0bee130de07a1
3s777 <3s777@rambler.ru>
2024-11-18 14:36:17 +0300

    add scroll buttons for responsive table

--- resources/views/components/ui/responsive-table/index.blade.php
+++ resources/views/components/ui/responsive-table/index.blade.php
@@ -2,7 +2,19 @@
     'footer' => false,
     'empty' => false,
 ])
+
+<div class="responsive-table__scroll-buttons responsive-table__scroll-buttons_static">
+    <x-ui.form.button class="responsive-table__scroll-button responsive-table__scroll-button_left" onclick="leftScroll()"></x-ui.form.button>
+    <x-ui.form.button class="responsive-table__scroll-button responsive-table__scroll-button_right" onclick="rightScroll()"></x-ui.form.button>
+</div>
+
 <div class="responsive-table__wrapper">
+        <div class="responsive-table__scroll-buttons responsive-table__scroll-buttons_fixed">
+            <x-ui.form.button class="responsive-table__scroll-button responsive-table__scroll-button_left" onclick="leftScroll()"></x-ui.form.button>
+            <x-ui.form.button class="responsive-table__scroll-button responsive-table__scroll-button_right" onclick="rightScroll()"></x-ui.form.button>
+        </div>
+
+
     <div
         {{ $attributes->class([
                 'responsive-table'
