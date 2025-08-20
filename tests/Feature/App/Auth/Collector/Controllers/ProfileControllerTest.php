5a4e67b83549164b5d692e8f155671b3a29d6537
3s777 <3s777@rambler.ru>
2025-08-20 21:03:43 +0300

    test: add profile tests

--- tests/Feature/App/Auth/Collector/Controllers/ProfileControllerTest.php
+++ tests/Feature/App/Auth/Collector/Controllers/ProfileControllerTest.php
@@ -28,19 +28,56 @@ class ProfileControllerTest extends TestCase
 //        $this->request = CreateCollectorRequest::factory()->create();
     }
 
+    public function checkNotAuthRedirect(
+        string $action,
+        string $method = 'get',
+        array $params = [],
+        array $request = []
+    ): void {
+        $this->{$method}(action([ProfileController::class, $action], $params), $request)
+            ->assertRedirectToRoute('collector.login');
+    }
+
+    public function test_pages_only_auth_success(): void
+    {
+        $this->checkNotAuthRedirect('show');
+        $this->checkNotAuthRedirect('settings');
+        $this->checkNotAuthRedirect('confidential');
+    }
+
     public function test_index_success(): void
     {
         $this->actingAs($this->authCollector, 'collector')
             ->get(action([ProfileController::class, 'show']))
             ->assertOk()
-            ->assertSee($this->authCollector->name)
+            ->assertSee($this->authCollector->first_name)
             ->assertSee(__('user.profile.info'))
             ->assertViewIs('content.profile.index');
     }
 
-    public function test_not_auth_failed(): void
+//    public function test_not_auth_failed(): void
+//    {
+//        $this->get(action([ProfileController::class, 'show']))
+//            ->assertRedirectToRoute('collector.login');
+//    }
+
+    public function test_settings_success(): void
     {
-        $this->get(action([ProfileController::class, 'show']))
-            ->assertRedirectToRoute('collector.login');
+        $this->actingAs($this->authCollector, 'collector')
+            ->get(action([ProfileController::class, 'settings']))
+            ->assertOk()
+            ->assertSee($this->authCollector->first_name)
+            ->assertSee($this->authCollector->email)
+            ->assertSee(__('user.profile.settings'))
+            ->assertViewIs('content.profile.settings');
+    }
+
+    public function test_confidential_success(): void
+    {
+        $this->actingAs($this->authCollector, 'collector')
+            ->get(action([ProfileController::class, 'confidential']))
+            ->assertOk()
+            ->assertSee(__('user.profile.settings_confidential'))
+            ->assertViewIs('content.profile.confidential');
     }
 }
