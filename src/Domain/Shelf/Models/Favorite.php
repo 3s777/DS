081f1b87b858484c78bf5bf5eb42011d2f18beab
3s777 <3s777@rambler.ru>
2025-10-22 11:08:50 +0300

    feat: add slug to Favorite

--- src/Domain/Shelf/Models/Favorite.php
+++ src/Domain/Shelf/Models/Favorite.php
@@ -4,9 +4,11 @@ namespace Domain\Shelf\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Support\Traits\Models\HasSlug;
 
 class Favorite extends Model
 {
     /** @use HasFactory<\Database\Factories\FavoriteFactory> */
     use HasFactory;
+    use HasSlug;
 }
