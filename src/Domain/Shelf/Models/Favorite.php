d5aa50f43a1e9419a38eea9b2de5895cf8fba576
3s777 <3s777@rambler.ru>
2025-10-22 11:07:56 +0300

    feat: add images to Favorite

--- src/Domain/Shelf/Models/Favorite.php
+++ src/Domain/Shelf/Models/Favorite.php
@@ -4,9 +4,21 @@ namespace Domain\Shelf\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Support\Traits\Models\HasImage;
 
 class Favorite extends Model
 {
     /** @use HasFactory<\Database\Factories\FavoriteFactory> */
     use HasFactory;
+    use HasImage;
+
+    protected function imagesDir(): string
+    {
+        // TODO: Implement imagesDir() method.
+    }
+
+    public function thumbnailSizes(): array
+    {
+        // TODO: Implement thumbnailSizes() method.
+    }
 }
