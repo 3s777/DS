cac51aaf8591119bada67b06a74561691d3a520d
3s777 <3s777@rambler.ru>
2025-10-22 11:36:24 +0300

    fix: remove all traits

--- src/Domain/Shelf/Models/Favorite.php
+++ src/Domain/Shelf/Models/Favorite.php
@@ -4,27 +4,9 @@ namespace Domain\Shelf\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Support\Traits\Models\HasCollector;
-use Support\Traits\Models\HasSlug;
-use Support\Traits\Models\HasImage;
-use Support\Traits\Models\HasUser;
-use Tests\Traits\HasFilters;
 
 class Favorite extends Model
 {
     /** @use HasFactory<\Database\Factories\FavoriteFactory> */
     use HasFactory;
-    use HasCollector;
-    use HasUser;
-
-
-    protected function imagesDir(): string
-    {
-        // TODO: Implement imagesDir() method.
-    }
-
-    public function thumbnailSizes(): array
-    {
-        // TODO: Implement thumbnailSizes() method.
-    }
 }
