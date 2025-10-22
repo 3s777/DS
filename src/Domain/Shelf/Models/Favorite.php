3f1dcac34ba8b7e378076684428b69d320b37b06
3s777 <3s777@rambler.ru>
2025-10-22 11:29:36 +0300

    feat: add Collector to Favorite

--- src/Domain/Shelf/Models/Favorite.php
+++ src/Domain/Shelf/Models/Favorite.php
@@ -4,14 +4,17 @@ namespace Domain\Shelf\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Support\Traits\Models\HasCollector;
 use Support\Traits\Models\HasSlug;
 use Support\Traits\Models\HasImage;
+use Tests\Traits\HasFilters;
 
 class Favorite extends Model
 {
     /** @use HasFactory<\Database\Factories\FavoriteFactory> */
     use HasFactory;
     use HasSlug;
+    use HasCollector;
     use HasImage;
 
     protected function imagesDir(): string
